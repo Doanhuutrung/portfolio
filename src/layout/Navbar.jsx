f9c80fc4e2552ca39c20094d3d4dbeb24bfd6f21
HuuTrung <huutrungdoan2202>
2026-03-09 14:25:58 +0700

    Update mobile's navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,4 +1,6 @@
 import { Button } from "@/components/Button";
+import { Menu } from "lucide-react";
+import { useState } from "react";
 
 const navLinks = [
   { href: "#about", label: "About" },
@@ -7,6 +9,7 @@ const navLinks = [
   { href: "#Testimonails", label: "Testimonails" },
 ];
 export const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   return (
     <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
       <nav className="container mx-auto px-6 flex items-center justify-between">
@@ -34,7 +37,30 @@ export const Navbar = () => {
           <Button size="sm">Contact Me</Button>
         </div>
         {/* Mobile Navbar */}
+        <button
+          className="md:hidden p-2 text-foreground"
+          onClick={() => setIsMobileMenuOpen((prev) => !prev)}
+        >
+          <Menu size={24} />
+        </button>
       </nav>
+      {isMobileMenuOpen && (
+        <div className="md:hidden glass-mobile">
+          <div className="container mx-auto px-6 py-6 flex flex-col gap-4">
+            {navLinks.map((link, index) => (
+              <a
+                href={link.href}
+                key={index}
+                className="text-lg text-muted-foreground hover:text-foreground py-2
+            "
+              >
+                {link.label}
+              </a>
+            ))}
+            <Button>Contact Me</Button>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
